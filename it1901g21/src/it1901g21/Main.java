87f3aef54e3d976172ff5b5d5fc5b4f57f31bf43
Liang <liang1503@live.no>
2013-10-02 13:37:37 +0200

    Hello world java

--- it1901g21/src/it1901g21/Main.java
+++ it1901g21/src/it1901g21/Main.java
@@ -3,7 +3,7 @@ package it1901g21;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World java");
     }
 
 }
