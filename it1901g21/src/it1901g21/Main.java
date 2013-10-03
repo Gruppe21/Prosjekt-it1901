291bccbe9dfcb8249b1b710cddf3f59fc6a59939
helliio <liang1503@live.no>
2013-10-03 14:59:01 +0200

    Update Main.java

--- it1901g21/src/it1901g21/Main.java
+++ it1901g21/src/it1901g21/Main.java
@@ -3,7 +3,7 @@ package it1901g21;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World. test push endret av wien");
+        System.out.println("Hello, World. test push endret av Li");
     }
 
 }
