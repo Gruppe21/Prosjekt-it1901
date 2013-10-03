78187e7755c3f0b06635d2ef2199bcfed8d83cbc
carnessir <andrewie@stud.ntnu.no>
2013-10-03 15:04:59 +0200

    added println

--- it1901g21/src/it1901g21/Main.java
+++ it1901g21/src/it1901g21/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World. test push endret av Li");
+        
+        System.out.print("Titus Pullo, by Wien");
     }
 
 }
