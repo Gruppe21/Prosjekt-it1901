1c48f0c8a94799b9b075222fd9352917715393a8
joachimo <joachim.oestensen@gmail.com>
2013-10-07 13:38:57 +0200

    :D

--- it1901g21/src/it1901g21/Main.java
+++ it1901g21/src/it1901g21/Main.java
@@ -3,11 +3,13 @@ package it1901g21;
 public class Main {
 
     public static void main(String[] args) {
+    	
         System.out.println("Hello, World. test push endret av Li");
         
         System.out.print("Titus Pullo, by Wien");
         
-        System.out.print("weeee!");
+        System.out.print("weeee! :D");
+        
     }
 
 }
