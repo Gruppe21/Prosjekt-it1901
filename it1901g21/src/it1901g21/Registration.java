2eb495b8fce3042b117099381f64e8c2349678e3
Liang <liang1503@live.no>
2013-10-28 14:38:51 +0100

    close update

--- it1901g21/src/it1901g21/Registration.java
+++ it1901g21/src/it1901g21/Registration.java
@@ -39,7 +39,7 @@ public class Registration extends JFrame {
 	 * Create the frame.
 	 */
 	public Registration() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 444, 374);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
