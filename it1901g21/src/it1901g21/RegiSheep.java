156fa97c5a33a06dea968f785faa1900fd7256d4
Liang <liang1503@live.no>
2013-11-04 22:31:20 +0100

    Registrere en sau

---
+++ it1901g21/src/it1901g21/RegiSheep.java
@@ -0,0 +1,81 @@
+package it1901g21;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+
+public class RegiSheep extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JButton btnNewButton;
+	private JButton btnAvbryt;
+
+	/**
+	 * Create the frame.
+	 */
+	public RegiSheep() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 237);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(191, 31, 209, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(191, 62, 209, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(191, 93, 209, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(191, 124, 209, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Sauenummer:");
+		lblNewLabel.setBounds(57, 34, 124, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblHelse = new JLabel("Helse:");
+		lblHelse.setBounds(57, 65, 124, 14);
+		contentPane.add(lblHelse);
+		
+		JLabel lblAlder = new JLabel("Alder:");
+		lblAlder.setBounds(57, 96, 124, 14);
+		contentPane.add(lblAlder);
+		
+		JLabel lblNvarendePosision = new JLabel("N\u00E5varende Posision:");
+		lblNvarendePosision.setBounds(57, 127, 124, 14);
+		contentPane.add(lblNvarendePosision);
+		
+		btnNewButton = new JButton("Ferdig");
+		btnNewButton.setBounds(300, 165, 124, 23);
+		contentPane.add(btnNewButton);
+		
+		btnAvbryt = new JButton("Avbryt");
+		btnAvbryt.setBounds(10, 165, 124, 23);
+		contentPane.add(btnAvbryt);
+		
+		this.setVisible(true);
+		
+	}
+}
