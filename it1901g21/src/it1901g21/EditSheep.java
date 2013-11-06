a39224b224817fd9e0ee7b58889aef549d6d0eee
Liang <liang1503@live.no>
2013-11-06 13:55:45 +0100

    Main window prototype

---
+++ it1901g21/src/it1901g21/EditSheep.java
@@ -0,0 +1,79 @@
+package it1901g21;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+
+public class EditSheep extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JButton btnNewButton;
+	private JButton btnAvbryt;
+	private JLabel lblSerialnumber;
+	private JLabel lblBirthdate;
+
+	/**
+	 * Create the frame.
+	 */
+	public EditSheep() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 243);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(191, 93, 209, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(191, 124, 209, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Sheep number");
+		lblNewLabel.setBounds(57, 34, 124, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblHelse = new JLabel("Birth Date yyyy.mm.dd:");
+		lblHelse.setBounds(57, 65, 124, 14);
+		contentPane.add(lblHelse);
+		
+		JLabel lblAlder = new JLabel("Health:");
+		lblAlder.setBounds(57, 96, 124, 14);
+		contentPane.add(lblAlder);
+		
+		btnNewButton = new JButton("Done");
+		btnNewButton.setBounds(300, 171, 124, 23);
+		contentPane.add(btnNewButton);
+		
+		btnAvbryt = new JButton("Cancel");
+		btnAvbryt.setBounds(10, 171, 124, 23);
+		contentPane.add(btnAvbryt);
+		
+		JLabel lblVekt = new JLabel("Weight:");
+		lblVekt.setBounds(57, 127, 124, 14);
+		contentPane.add(lblVekt);
+		
+		lblSerialnumber = new JLabel("SerialNumber");
+		lblSerialnumber.setBounds(191, 34, 209, 14);
+		contentPane.add(lblSerialnumber);
+		
+		lblBirthdate = new JLabel("BirthDate");
+		lblBirthdate.setBounds(191, 65, 209, 14);
+		contentPane.add(lblBirthdate);
+		
+		this.setVisible(true);
+		
+	}
+}
---
+++ it1901g21/src/it1901g21/MainScreen.java
@@ -0,0 +1,50 @@
+package it1901g21;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
+
+
+public class MainScreen extends JFrame {
+
+	private JPanel contentPane;
+	
+	/**
+	 * Create the frame.
+	 */
+	public MainScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 691, 529);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(297, 55, 368, 349);
+		contentPane.add(list);
+		
+		JButton btnNewButton = new JButton("Add Sheep");
+		btnNewButton.setBounds(38, 99, 89, 45);
+		contentPane.add(btnNewButton);
+		
+		JButton btnDelSheep = new JButton("Del Sheep");
+		btnDelSheep.setBounds(152, 99, 89, 45);
+		contentPane.add(btnDelSheep);
+		
+		JButton btnProfile = new JButton("Profile");
+		btnProfile.setBounds(38, 155, 89, 45);
+		contentPane.add(btnProfile);
+		
+		JButton btnAlarm = new JButton("Alarm");
+		btnAlarm.setBackground(Color.RED);
+		btnAlarm.setBounds(152, 155, 89, 45);
+		contentPane.add(btnAlarm);
+	}
+}
