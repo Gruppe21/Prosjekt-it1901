24c4c7d6abc7571d3c5b1968c4529cd5d5db1612
Andreas Oeium <and.oe@outlook.com>
2013-11-08 01:15:43 +0100

    Fixed password hashing, maybe...

--- it1901g21/src/database/PasswordHash.java
+++ it1901g21/src/database/PasswordHash.java
@@ -20,20 +20,50 @@ public class PasswordHash {
 		byte[] salt = new byte[SALT_BYTE_SIZE];
 		randomSalt.nextBytes(salt);
 		
+		return generateHash(password, salt); 
+	}
+	
+	/**
+	 * Checks if given password is correct.
+	 * @param password the given password.
+	 * @param usedSalt the salt used for the correct password hash.
+	 * @param correctHash the correct password hash to compare.
+	 * @return true if correct
+	 */
+	public boolean isValidated(String password, String usedSalt, String correctHash) {
+		
+		// Generates the password hash with the chosen salt
+		byte[] salt = usedSalt.getBytes();
+		String[] hashAndSalt = generateHash(password, salt);
+		
+		// Checks if the password hashes are the same
+		if (hashAndSalt[0].equals(correctHash))
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Creates a password hash from given password and salt.
+	 * @param password the password to hash
+	 * @param salt the salt to use
+	 * @return the hash and salt
+	 */
+	private String[] generateHash(String password, byte[] salt) {
+		
 		try {
 			PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 1000, SALT_BYTE_SIZE * 8);
 			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 			byte[] hash = skf.generateSecret(spec).getEncoded();
-			String hashString = convertToHex(hash);
-			String saltString = convertToHex(salt);
 			
-			return new String[] { hashString, saltString };
+			return new String[] { convertToHex(hash), convertToHex(salt) };
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		return null;
+		
 	}
 	
 	/**
