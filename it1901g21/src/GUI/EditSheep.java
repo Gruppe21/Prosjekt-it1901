cac9f129b182ca11f349a3fe38a64bcfb56c00ad
Liang <liang1503@live.no>
2013-11-15 16:42:30 +0100

    update edit sheep

--- it1901g21/src/GUI/EditSheep.java
+++ it1901g21/src/GUI/EditSheep.java
@@ -25,53 +25,65 @@ public class EditSheep extends JFrame {
 	 */
 	public EditSheep() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 243);
+		setBounds(100, 100, 450, 287);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(191, 93, 209, 20);
+		textField_2.setBounds(223, 118, 177, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(191, 124, 209, 20);
+		textField_3.setBounds(223, 151, 177, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Sheep number");
+		JLabel lblNewLabel = new JLabel("Sheep number:");
 		lblNewLabel.setBounds(57, 34, 124, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblHelse = new JLabel("Birth Date (yyyy.mm.dd):");
-		lblHelse.setBounds(57, 65, 124, 14);
+		lblHelse.setBounds(57, 65, 159, 14);
 		contentPane.add(lblHelse);
 		
 		JLabel lblAlder = new JLabel("Health:");
-		lblAlder.setBounds(57, 96, 124, 14);
+		lblAlder.setBounds(57, 121, 124, 14);
 		contentPane.add(lblAlder);
 		
 		btnNewButton = new JButton("Done");
-		btnNewButton.setBounds(300, 171, 124, 23);
+		btnNewButton.setBounds(296, 204, 124, 23);
 		contentPane.add(btnNewButton);
 		
 		btnAvbryt = new JButton("Cancel");
-		btnAvbryt.setBounds(10, 171, 124, 23);
+		btnAvbryt.setBounds(12, 204, 124, 23);
 		contentPane.add(btnAvbryt);
 		
 		JLabel lblVekt = new JLabel("Weight (kg):");
-		lblVekt.setBounds(57, 127, 124, 14);
+		lblVekt.setBounds(57, 148, 124, 14);
 		contentPane.add(lblVekt);
 		
 		lblSerialnumber = new JLabel("SerialNumber");
-		lblSerialnumber.setBounds(191, 34, 209, 14);
+		lblSerialnumber.setBounds(223, 34, 177, 14);
 		contentPane.add(lblSerialnumber);
 		
 		lblBirthdate = new JLabel("BirthDate");
-		lblBirthdate.setBounds(191, 65, 209, 14);
+		lblBirthdate.setBounds(223, 65, 177, 14);
 		contentPane.add(lblBirthdate);
+		
+		JLabel lblLocation = new JLabel("Location:");
+		lblLocation.setBounds(57, 92, 56, 16);
+		contentPane.add(lblLocation);
+		
+		JLabel lblXpos = new JLabel("xpos");
+		lblXpos.setBounds(223, 92, 56, 16);
+		contentPane.add(lblXpos);
+		
+		JLabel lblYpos = new JLabel("ypos");
+		lblYpos.setBounds(291, 92, 56, 16);
+		contentPane.add(lblYpos);
 		
 		this.setVisible(true);
 		
