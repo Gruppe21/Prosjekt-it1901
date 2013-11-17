30fd60010c420c438139c9724e0be168c4dc1198
Liang <liang1503@live.no>
2013-11-17 17:26:26 +0100

    Alarm window added

---
+++ it1901g21/src/GUI/Alarm.java
@@ -0,0 +1,94 @@
+package GUI;
+
+import it1901g21.Main;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Alarm {
+	
+	private Main main;
+	
+	private JFrame frame;
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public Alarm(Main main) {
+		
+		this.main = main;
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JLabel lblPleaseEnterSheep = new JLabel("Send Email?");
+		springLayout.putConstraint(SpringLayout.NORTH, lblPleaseEnterSheep, 66, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblPleaseEnterSheep, -149, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(lblPleaseEnterSheep);
+		
+		JButton btnConfirm = new JButton("Yes");
+		springLayout.putConstraint(SpringLayout.NORTH, btnConfirm, 101, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnConfirm, -27, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnConfirm, -42, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(btnConfirm);
+		
+		JButton btnCancel = new JButton("No");
+		springLayout.putConstraint(SpringLayout.NORTH, btnCancel, 101, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnConfirm, 87, SpringLayout.EAST, btnCancel);
+		springLayout.putConstraint(SpringLayout.WEST, btnCancel, 47, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnCancel, 150, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(btnCancel);
+		
+		/*
+		 * Action to happen when user clicks "cancel"
+		 */
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				closeAlarm();
+			}
+		});
+		
+		/*
+		 * Action to happen when user clicks "confirm"
+		 */
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+			}
+		});
+		
+	}
+	
+	
+	/**
+	 * Opens the delSheep window
+	 */
+	public void openAlarm() {
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Closes the delSheep window
+	 */
+	private void closeAlarm(){
+		frame.setVisible(false);
+	}
+	
+	/**
+	 * Sends the prompt to delete the sheep to Main
+	 */
+	private void sendAlarm(String earTag) {
+	}
+}
