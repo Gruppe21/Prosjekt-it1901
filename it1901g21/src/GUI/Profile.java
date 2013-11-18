3ae55a4613d97a7c23bf2e0bd84a93b414d38622
Liang <liang1503@live.no>
2013-11-18 13:55:47 +0100

    All windows are now absouloute

--- it1901g21/src/GUI/Profile.java
+++ it1901g21/src/GUI/Profile.java
@@ -27,37 +27,31 @@ public class Profile {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 300, 259);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		SpringLayout springLayout = new SpringLayout();
-		frame.getContentPane().setLayout(springLayout);
 		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
 		
 		JLabel lblFirstName = new JLabel("Name:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblFirstName, 31, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, lblFirstName, 10, SpringLayout.WEST, frame.getContentPane());
+		lblFirstName.setBounds(10, 31, 38, 16);
 		frame.getContentPane().add(lblFirstName);
 		
 		JLabel lblEmail = new JLabel("E-mail:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblEmail, 6, SpringLayout.SOUTH, lblFirstName);
-		springLayout.putConstraint(SpringLayout.WEST, lblEmail, 0, SpringLayout.WEST, lblFirstName);
+		lblEmail.setBounds(10, 53, 41, 16);
 		frame.getContentPane().add(lblEmail);
 		
 		JLabel lblPhoneNumber = new JLabel("Phone number:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblPhoneNumber, 6, SpringLayout.SOUTH, lblEmail);
-		springLayout.putConstraint(SpringLayout.WEST, lblPhoneNumber, 0, SpringLayout.WEST, lblFirstName);
+		lblPhoneNumber.setBounds(10, 75, 88, 16);
 		frame.getContentPane().add(lblPhoneNumber);
 		
 		JLabel lblContactPersonName = new JLabel("Contact mail:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblContactPersonName, 6, SpringLayout.SOUTH, lblPhoneNumber);
-		springLayout.putConstraint(SpringLayout.WEST, lblContactPersonName, 0, SpringLayout.WEST, lblFirstName);
+		lblContactPersonName.setBounds(10, 97, 76, 16);
 		frame.getContentPane().add(lblContactPersonName);
 		
 		JLabel lblContactPhone = new JLabel("Contact Phone:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblContactPhone, 6, SpringLayout.SOUTH, lblContactPersonName);
-		springLayout.putConstraint(SpringLayout.WEST, lblContactPhone, 0, SpringLayout.WEST, lblFirstName);
+		lblContactPhone.setBounds(10, 119, 87, 16);
 		frame.getContentPane().add(lblContactPhone);
 		
 		JButton btnDone = new JButton("Done");
-		springLayout.putConstraint(SpringLayout.SOUTH, btnDone, -10, SpringLayout.SOUTH, frame.getContentPane());
+		btnDone.setBounds(205, 189, 61, 25);
 		frame.getContentPane().add(btnDone);
 		frame.getRootPane().setDefaultButton(btnDone);
 		
@@ -68,41 +62,31 @@ public class Profile {
 		});
 		
 		JLabel lblname = new JLabel(farmer.getName());
-		springLayout.putConstraint(SpringLayout.EAST, btnDone, 0, SpringLayout.EAST, lblname);
-		springLayout.putConstraint(SpringLayout.NORTH, lblname, 0, SpringLayout.NORTH, lblFirstName);
-		springLayout.putConstraint(SpringLayout.WEST, lblname, 6, SpringLayout.EAST, lblFirstName);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblname, 0, SpringLayout.SOUTH, lblFirstName);
-		springLayout.putConstraint(SpringLayout.EAST, lblname, -28, SpringLayout.EAST, frame.getContentPane());
+		lblname.setBounds(54, 31, 212, 16);
 		frame.getContentPane().add(lblname);
 		
 		JLabel lblGetLastName = new JLabel();
-		springLayout.putConstraint(SpringLayout.NORTH, lblGetLastName, 6, SpringLayout.SOUTH, lblname);
-		springLayout.putConstraint(SpringLayout.WEST, lblGetLastName, 73, SpringLayout.WEST, frame.getContentPane());
+		lblGetLastName.setBounds(73, 53, 0, 0);
 		frame.getContentPane().add(lblGetLastName);
 		
 		JLabel lvlGetEmail = new JLabel(farmer.getMail());
-		springLayout.putConstraint(SpringLayout.NORTH, lvlGetEmail, 0, SpringLayout.NORTH, lblEmail);
-		springLayout.putConstraint(SpringLayout.WEST, lvlGetEmail, 6, SpringLayout.EAST, lblEmail);
+		lvlGetEmail.setBounds(57, 53, 65, 16);
 		frame.getContentPane().add(lvlGetEmail);
 		
 		JLabel lblGetTlf = new JLabel(farmer.getTlf());
-		springLayout.putConstraint(SpringLayout.NORTH, lblGetTlf, 0, SpringLayout.NORTH, lblPhoneNumber);
-		springLayout.putConstraint(SpringLayout.WEST, lblGetTlf, 8, SpringLayout.EAST, lblPhoneNumber);
+		lblGetTlf.setBounds(106, 75, 65, 16);
 		frame.getContentPane().add(lblGetTlf);
 		
 		JLabel lblgetContactname = new JLabel(farmer.getResMail());
-		springLayout.putConstraint(SpringLayout.NORTH, lblgetContactname, 6, SpringLayout.SOUTH, lblPhoneNumber);
-		springLayout.putConstraint(SpringLayout.WEST, lblgetContactname, 6, SpringLayout.EAST, lblContactPersonName);
+		lblgetContactname.setBounds(92, 97, 65, 16);
 		frame.getContentPane().add(lblgetContactname);
 		
 		JLabel lblgetContactPhone = new JLabel(farmer.getResTlf());
-		springLayout.putConstraint(SpringLayout.NORTH, lblgetContactPhone, 0, SpringLayout.NORTH, lblContactPhone);
-		springLayout.putConstraint(SpringLayout.WEST, lblgetContactPhone, 6, SpringLayout.EAST, lblContactPhone);
+		lblgetContactPhone.setBounds(103, 119, 65, 16);
 		frame.getContentPane().add(lblgetContactPhone);
 		
 		JButton btnEdit = new JButton("Edit");
-		springLayout.putConstraint(SpringLayout.NORTH, btnEdit, 0, SpringLayout.NORTH, btnDone);
-		springLayout.putConstraint(SpringLayout.EAST, btnEdit, 0, SpringLayout.EAST, lblContactPersonName);
+		btnEdit.setBounds(25, 189, 61, 25);
 		frame.getContentPane().add(btnEdit);
 		
 		btnEdit.addActionListener(new ActionListener() {
